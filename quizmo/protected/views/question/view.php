bb42dc5e2f0583d9dbc54dd8a21ad546858bbba5
jazahn <jcleveng@fas.harvard.edu>
2012-05-28 22:55:56 -0400

    fixed view to display without errors...

--- quizmo/protected/views/question/view.php
+++ quizmo/protected/views/question/view.php
@@ -1,31 +1,31 @@
 <?php
 $this->breadcrumbs=array(
 	'Questions'=>array('index'),
-	$model->title,
+	$model->TITLE,
 );
 
 $this->menu=array(
 	array('label'=>'List Question', 'url'=>array('index')),
 	array('label'=>'Create Question', 'url'=>array('create')),
-	array('label'=>'Update Question', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Question', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Update Question', 'url'=>array('update', 'id'=>$model->ID)),
+	array('label'=>'Delete Question', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->ID),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage Question', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View Question #<?php echo $model->id; ?></h1>
+<h1>View Question #<?php echo $model->ID; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
-		'id',
-		'quiz_id',
-		'question_type',
-		'title',
-		'body',
-		'question_order',
-		'points',
-		'feedback',
-		'deleted',
+		'ID',
+		'QUIZ_ID',
+		'QUESTION_TYPE',
+		'TITLE',
+		'BODY',
+		'QUESTION_ORDER',
+		'POINTS',
+		'FEEDBACK',
+		'DELETED',
 	),
 )); ?>
