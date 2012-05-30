75221ab36552448f25fdf1db03c3b1723c1fb150
jazahn <jcleveng@fas.harvard.edu>
2012-05-29 21:44:17 -0400

    added url to main layout and got it working for non isites

--- quizmo/js/url.js
+++ quizmo/js/url.js
@@ -1,5 +1,5 @@
 function url(url){
-	if(Isites != undefined){
+	if(typeof Isites !== "undefined"){
 		// these are set in the isites layout (views/layouts/isites)
 		url = url + "?pageContentId=" + pageContentId + "&topicId=" + topicId;
 		url = Isites.constructUrl(url);
--- quizmo/protected/views/layouts/main.php
+++ quizmo/protected/views/layouts/main.php
@@ -30,6 +30,8 @@
     <link rel="apple-touch-icon-precomposed" href="/ico/apple-touch-icon-57-precomposed.png">
     <script src="/js/jquery-1.7.2.min.js"></script>
     <script src="/js/bootstrap.js"></script>
+    <script src="/js/url.js"></script>
+
   </head>
 
   <body>
