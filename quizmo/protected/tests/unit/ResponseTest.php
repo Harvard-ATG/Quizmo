e6a50e66fb8e9b33815b0d9d2c068e40c2951772
jazahn <jcleveng@fas.harvard.edu>
2012-07-08 21:59:04 -0400

    added ResponseTest

---
+++ quizmo/protected/tests/unit/ResponseTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* ResponseTest unit test
+*
+* 
+*/
+class ResponseTest extends CDbTestCase {
+   
+	
+
+	public function testSubmitEssayQuestion(){
+		$question_id = 1;
+		$user_id = 4;
+		$response_text = "this is an essay";
+
+		// first make sure it doesn't already exist
+		$response = Response::model()->find('user_id=:user_id AND question_id=:question_id', 
+			array(
+				':user_id' => $user_id,			
+				':question_id' => $question_id,			
+			)
+		);
+		$this->assertNull($response);
+		
+		// then submit the question
+		Response::submitEssayQuestion($user_id, $question_id, $response_text);
+		
+		// then check that it's there
+		// with the type set
+		// with only one
+		
+		// then delete
+		
+
+	}
+	
+
+
+
+   
+}
