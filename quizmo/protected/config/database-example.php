314547438974eecd01b5960edf499f788c8d70ed
jazahn <jcleveng@fas.harvard.edu>
2012-07-10 22:25:53 -0400

    added database-example

---
+++ quizmo/protected/config/database-example.php
@@ -0,0 +1,30 @@
+<?php
+
+return array(
+	'components'=>array(
+
+		// uncomment the following to use an Oracle database
+		/*
+		'db'=>array(
+			'class' => 'CDbConnection',
+			'connectionString' => 'oci:dbname=my_host;charset=UTF8',
+			'username' => 'my_username',
+			'password' => 'my_password',
+			//'schemaCachingDuration' => 1000000,
+		),
+		*/
+
+		'db'=>array(
+			'class' => 'CDbConnection',
+			'connectionString' => 'mysql:host=localhost;dbname=quizmo_dev',
+			'emulatePrepare' => true,
+			'username' => 'quizmo_dev',
+			'password' => 'quizmo_dev',
+			'charset' => 'utf8',
+		),
+
+	),
+);
+
+
+?>
