7d547ce125f85580441c9900292834c0a91d30bb
jazahn <jcleveng@fas.harvard.edu>
2012-07-13 12:44:13 -0400

    isites merge -- database-example file

--- quizmo/protected/config/database-example.php
+++ quizmo/protected/config/database-example.php
@@ -4,21 +4,16 @@ return array(
 	'components'=>array(
 
 		// uncomment the following to use an Oracle database
+		/*
 		'db'=>array(
 			'class' => 'CDbConnection',
-			'connectionString' => 'oci:dbname=icgdbdev.fas.harvard.edu;charset=UTF8',
-			'username' => 'quizmo_dev',
-			'password' => 'YcKx_2xCbm',
+			'connectionString' => 'oci:dbname=myDBname;charset=UTF8',
+			'username' => 'username',
+			'password' => 'password',
 			//'schemaCachingDuration' => 1000000,
 		),
-<<<<<<< HEAD
 		*/
-
-=======
 		
-		
-		/*
->>>>>>> 3f5ad3577ebe3f5c3c93c7f93eb9dc74c692ac1d
 		'db'=>array(
 			'class' => 'CDbConnection',
 			'connectionString' => 'mysql:host=localhost;dbname=quizmo_dev',
@@ -27,12 +22,7 @@ return array(
 			'password' => 'quizmo_dev',
 			'charset' => 'utf8',
 		),
-<<<<<<< HEAD
-
-=======
-		*/
 				
->>>>>>> 3f5ad3577ebe3f5c3c93c7f93eb9dc74c692ac1d
 	),
 );
 
