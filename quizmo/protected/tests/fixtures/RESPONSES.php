ef524df91236bd17098e61eb9be2349ff9253541
jazahn <jcleveng@fas.harvard.edu>
2012-07-14 23:24:32 -0400

    added responses fixture

---
+++ quizmo/protected/tests/fixtures/RESPONSES.php
@@ -0,0 +1,357 @@
+<?php
+/**
+ * These are the fixtures for table "RESPONSES".
+ *
+ * The followings are the available columns in table 'RESPONSES':
+ * @property integer $ID
+ * @property integer $QUESTION_ID
+ * @property string $QUESTION_TYPE
+ * @property integer $USER_ID
+ * @property string $RESPONSE
+ * @property string $SCORE_STATE
+ * @property integer $SCORE
+ * @property string $DATE_MODIFIED
+ * @property integer $MODIFIED_BY
+ */
+return array(
+	
+	'responses1' => array(
+		'QUESTION_ID' => '1',
+		'QUESTION_TYPE' => 'M',
+		'USER_ID' => 2,
+		'RESPONSE' => 1,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses2' => array(
+		'QUESTION_ID' => '2',
+		'QUESTION_TYPE' => 'T',
+		'USER_ID' => 2,
+		'RESPONSE' => 4,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses3' => array(
+		'QUESTION_ID' => '3',
+		'QUESTION_TYPE' => 'N',
+		'USER_ID' => 2,
+		'RESPONSE' => 1,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses4' => array(
+		'QUESTION_ID' => '4',
+		'QUESTION_TYPE' => 'N',
+		'USER_ID' => 2,
+		'RESPONSE' => 2,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses5' => array(
+		'QUESTION_ID' => '5',
+		'QUESTION_TYPE' => 'E',
+		'USER_ID' => 2,
+		'RESPONSE' => 'I am the very model of a modern major general',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses6a' => array(
+		'QUESTION_ID' => '6',
+		'QUESTION_TYPE' => 'S',
+		'USER_ID' => 2,
+		'RESPONSE' => 9,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses6b' => array(
+		'QUESTION_ID' => '6',
+		'QUESTION_TYPE' => 'S',
+		'USER_ID' => 2,
+		'RESPONSE' => 10,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses7' => array(
+		'QUESTION_ID' => '7',
+		'QUESTION_TYPE' => 'E',
+		'USER_ID' => 2,
+		'RESPONSE' => '',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses8a' => array(
+		'QUESTION_ID' => '8',
+		'QUESTION_TYPE' => 'F',
+		'USER_ID' => 2,
+		'RESPONSE' => 'lime',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses8b' => array(
+		'QUESTION_ID' => '8',
+		'QUESTION_TYPE' => 'F',
+		'USER_ID' => 2,
+		'RESPONSE' => 'coconut',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses8c' => array(
+		'QUESTION_ID' => '8',
+		'QUESTION_TYPE' => 'F',
+		'USER_ID' => 2,
+		'RESPONSE' => 'drink',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+
+	// user_id 3
+	'responses-3-1' => array(
+		'QUESTION_ID' => '1',
+		'QUESTION_TYPE' => 'M',
+		'USER_ID' => 3,
+		'RESPONSE' => 1,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-3-2' => array(
+		'QUESTION_ID' => '2',
+		'QUESTION_TYPE' => 'T',
+		'USER_ID' => 3,
+		'RESPONSE' => 4,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-3-3' => array(
+		'QUESTION_ID' => '3',
+		'QUESTION_TYPE' => 'N',
+		'USER_ID' => 3,
+		'RESPONSE' => 1,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-3-4' => array(
+		'QUESTION_ID' => '4',
+		'QUESTION_TYPE' => 'N',
+		'USER_ID' => 3,
+		'RESPONSE' => 2,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-3-5' => array(
+		'QUESTION_ID' => '5',
+		'QUESTION_TYPE' => 'E',
+		'USER_ID' => 3,
+		'RESPONSE' => 'I am the very model of a modern major general',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-3-6a' => array(
+		'QUESTION_ID' => '6',
+		'QUESTION_TYPE' => 'S',
+		'USER_ID' => 3,
+		'RESPONSE' => 9,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-3-6b' => array(
+		'QUESTION_ID' => '6',
+		'QUESTION_TYPE' => 'S',
+		'USER_ID' => 3,
+		'RESPONSE' => 10,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-3-7' => array(
+		'QUESTION_ID' => '7',
+		'QUESTION_TYPE' => 'E',
+		'USER_ID' => 3,
+		'RESPONSE' => '',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-3-8a' => array(
+		'QUESTION_ID' => '8',
+		'QUESTION_TYPE' => 'F',
+		'USER_ID' => 3,
+		'RESPONSE' => 'lime',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-3-8b' => array(
+		'QUESTION_ID' => '8',
+		'QUESTION_TYPE' => 'F',
+		'USER_ID' => 3,
+		'RESPONSE' => 'coconut',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-3-8c' => array(
+		'QUESTION_ID' => '8',
+		'QUESTION_TYPE' => 'F',
+		'USER_ID' => 3,
+		'RESPONSE' => 'drink',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	
+	// user_id 4
+	'responses-4-1' => array(
+		'QUESTION_ID' => '1',
+		'QUESTION_TYPE' => 'M',
+		'USER_ID' => 4,
+		'RESPONSE' => 1,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-4-2' => array(
+		'QUESTION_ID' => '2',
+		'QUESTION_TYPE' => 'T',
+		'USER_ID' => 4,
+		'RESPONSE' => 4,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-4-3' => array(
+		'QUESTION_ID' => '3',
+		'QUESTION_TYPE' => 'N',
+		'USER_ID' => 4,
+		'RESPONSE' => 1,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-4-4' => array(
+		'QUESTION_ID' => '4',
+		'QUESTION_TYPE' => 'N',
+		'USER_ID' => 4,
+		'RESPONSE' => 2,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-4-5' => array(
+		'QUESTION_ID' => '5',
+		'QUESTION_TYPE' => 'E',
+		'USER_ID' => 4,
+		'RESPONSE' => 'I am the very model of a modern major general',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-4-6a' => array(
+		'QUESTION_ID' => '6',
+		'QUESTION_TYPE' => 'S',
+		'USER_ID' => 4,
+		'RESPONSE' => 9,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-4-6b' => array(
+		'QUESTION_ID' => '6',
+		'QUESTION_TYPE' => 'S',
+		'USER_ID' => 4,
+		'RESPONSE' => 10,
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-4-7' => array(
+		'QUESTION_ID' => '7',
+		'QUESTION_TYPE' => 'E',
+		'USER_ID' => 4,
+		'RESPONSE' => '',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-4-8a' => array(
+		'QUESTION_ID' => '8',
+		'QUESTION_TYPE' => 'F',
+		'USER_ID' => 4,
+		'RESPONSE' => 'lime',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-4-8b' => array(
+		'QUESTION_ID' => '8',
+		'QUESTION_TYPE' => 'F',
+		'USER_ID' => 4,
+		'RESPONSE' => 'coconut',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	'responses-4-8c' => array(
+		'QUESTION_ID' => '8',
+		'QUESTION_TYPE' => 'F',
+		'USER_ID' => 4,
+		'RESPONSE' => 'drink',
+		'SCORE_STATE' => 'U',
+		'SCORE' => 0,
+		'DATE_MODIFIED' => '',
+		'MODIFIED_BY' => ''
+	),
+	
+	
+);
+
+
+?>
