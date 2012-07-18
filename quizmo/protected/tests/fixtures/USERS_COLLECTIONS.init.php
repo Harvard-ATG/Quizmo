5d387e8d6ba9a6f88300ac4acff6066bf92090ab
jazahn <jcleveng@fas.harvard.edu>
2012-07-17 23:30:04 -0400

    made sure the last run test doesn't remove all the fixtures

--- quizmo/protected/tests/fixtures/USERS_COLLECTIONS.init.php
+++ quizmo/protected/tests/fixtures/USERS_COLLECTIONS.init.php
@@ -1,9 +1,9 @@
 <?php
-$this->truncateTable('SUBMISSIONS');
-$this->truncateTable('RESPONSES');
+//$this->truncateTable('SUBMISSIONS');
+//$this->truncateTable('RESPONSES');
 $this->truncateTable('USERS_COLLECTIONS');
-$this->truncateTable('ANSWERS');
-$this->truncateTable('QUESTIONS');
+//$this->truncateTable('ANSWERS');
+//$this->truncateTable('QUESTIONS');
 //$this->truncateTable('QUIZES');
 //$this->truncateTable('USERS');
 //$this->truncateTable('COLLECTIONS');
