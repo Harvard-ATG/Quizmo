c90bd1093118bde949090ecae6fcb6dd47051fcd
jazahn <jcleveng@fas.harvard.edu>
2012-07-21 19:43:51 -0400

    added a very large response to the fixtures 3000bytes

--- quizmo/protected/tests/fixtures/RESPONSES.php
+++ quizmo/protected/tests/fixtures/RESPONSES.php
@@ -59,7 +59,19 @@ return array(
 		'QUESTION_ID' => '5',
 		'QUESTION_TYPE' => 'E',
 		'USER_ID' => 2,
-		'RESPONSE' => 'I am the very model of a modern major general',
+		'RESPONSE' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris scelerisque augue sit amet sem lobortis blandit. Proin vestibulum lorem sed nunc sodales a dapibus elit eleifend. Nulla dui justo, fringilla non consequat at, suscipit vitae est. Nam vel ante tortor, non feugiat augue. Donec blandit elit eget nisi tincidunt quis aliquet ipsum molestie. Vestibulum nec risus dolor. Curabitur quam metus, pulvinar vitae hendrerit quis, volutpat eget nulla. Praesent scelerisque tempus rhoncus. Praesent iaculis purus vel tellus porta eget imperdiet dolor porttitor. Mauris suscipit pharetra auctor.
+
+		Cras commodo tellus in neque malesuada mattis. Nullam ullamcorper lorem et diam aliquam vel blandit quam dapibus. Pellentesque condimentum, purus ornare iaculis egestas, turpis lorem rhoncus tellus, ac hendrerit lorem lectus vitae felis. Integer at felis est. Praesent porttitor congue elit, sed scelerisque mi venenatis vel. Maecenas hendrerit facilisis vehicula. Sed ut ipsum eu justo pharetra semper et nec arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+		Duis libero diam, convallis et pellentesque et, gravida eget risus. Vivamus consequat quam et lorem lacinia elementum porttitor nunc congue. Nullam non enim egestas risus iaculis sollicitudin. Curabitur vitae ligula quis magna suscipit interdum. Sed justo elit, eleifend et malesuada laoreet, venenatis sed eros. Curabitur commodo convallis iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas quis fermentum velit.
+
+		Fusce ac nisi hendrerit orci laoreet tincidunt a nec nisi. Nam metus nibh, sollicitudin quis blandit sit amet, mattis eu lacus. Nam rutrum sapien vel felis porta ac tempus nisi ullamcorper. Suspendisse convallis suscipit ligula eu laoreet. In quis sapien quis eros vehicula sollicitudin in nec tortor. Proin tempus mollis tortor, eleifend scelerisque lectus laoreet non. Nam egestas tincidunt fermentum. Etiam congue auctor nunc.
+
+		Ut odio lacus, convallis eu dapibus ac, accumsan vel turpis. Maecenas risus dui, ultrices nec condimentum eu, feugiat sed libero. Maecenas bibendum porttitor tellus ut fermentum. Integer sed sapien vitae leo dictum imperdiet at ut nisi. Mauris magna lectus, dapibus non malesuada et, suscipit dignissim lacus. Phasellus tincidunt convallis lobortis. Ut in nisl tellus. Quisque porta pretium leo. Aliquam nec felis vitae ante molestie condimentum consectetur et massa.
+
+		Praesent nec nulla nisl, eu mattis diam. Praesent non ligula eget enim consequat placerat id at lacus. Pellentesque adipiscing egestas risus in congue. Fusce pulvinar imperdiet leo, at fermentum lorem viverra ut. Vivamus nunc elit, sagittis feugiat congue quis, dapibus placerat diam. Maecenas vestibulum tortor ut nunc gravida venenatis. Integer hendrerit posuere quam. Sed massa est, egestas quis vulputate vel, suscipit at enim.
+
+		Vestibulum sagittis nunc ut ante laoreet suscipit. Aenean consectetur nunc vitae nulla lobortis posuere.',
 		'SCORE_STATE' => 'U',
 		'SCORE' => 1,
 		'DATE_MODIFIED' => '',
