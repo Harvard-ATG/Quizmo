1827f6c42d0a1de5c4eb7953a725a310b8d93012
jazahn <jcleveng@fas.harvard.edu>
2012-08-06 10:25:15 -0400

    cleaning up site controller

--- quizmo/protected/controllers/SiteController.php
+++ quizmo/protected/controllers/SiteController.php
@@ -31,7 +31,7 @@ class SiteController extends Controller
 	 */
 	public function actionIndex()
 	{
-		error_log("site/index");
+		//error_log("site/index");
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
 		//$this->render('index');
@@ -53,42 +53,6 @@ class SiteController extends Controller
 		}
 		
 	}
-
-	/**
-	 * This is the action to handle external exceptions.
-	 */
-	public function actionError()
-	{
-	    if($error=Yii::app()->errorHandler->error)
-	    {
-	    	if(Yii::app()->request->isAjaxRequest)
-	    		echo $error['message'];
-	    	else
-	        	$this->render('error', $error);
-	    }
-	}
-
-	/**
-	 * Displays the contact page
-	 */
-	public function actionContact()
-	{
-		$model=new ContactForm;
-		if(isset($_POST['ContactForm']))
-		{
-			$model->attributes=$_POST['ContactForm'];
-			if($model->validate())
-			{
-				$headers="From: {$model->email}\r\nReply-To: {$model->email}";
-				mail(Yii::app()->params['adminEmail'],$model->subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
-				$this->refresh();
-			}
-		}
-		$this->render('contact',array('model'=>$model));
-	}
-	
-	
 	
 	/**
 	 * Displays the login page
