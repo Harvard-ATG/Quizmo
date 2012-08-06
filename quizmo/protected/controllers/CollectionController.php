77427e4a1919b2fa8dda75ce9e5a5dbec9386e07
jazahn <jcleveng@fas.harvard.edu>
2012-08-06 10:15:15 -0400

    role base authentication, cleaned out unused leftovers from gii

--- quizmo/protected/controllers/CollectionController.php
+++ quizmo/protected/controllers/CollectionController.php
@@ -31,28 +31,13 @@ class CollectionController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','edit'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'actions'=>array('create','update','edit','admin','delete'),
+				'roles'=>array('admin','super'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
 			),
 		);
-	}
-
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id)
-	{
-		$this->render('view',array(
-			'collection'=>$this->loadModel($id),
-		));
 	}
 
 	/**
@@ -123,67 +108,8 @@ class CollectionController extends Controller
 
 	}
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id='')
-	{
-		
-		$model=$this->loadModel($id);
-		
-		if($id == ''){
-			// then this is a create action
-			$title = Yii::app()->getRequest()->getParam('title');
-			$description = Yii::app()->getRequest()->getParam('description');
-			if($title != ''){
-				
-			}
-			$model->create();
-			
-		} else {
-			
-			
-			
-		}
-		
-
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
-
-		if(isset($_POST['Collection']))
-		{
-			$model->attributes=$_POST['Collection'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->ID));
-		}
-
-		$this->render('update',array(
-			'model'=>$model,
-		));
 	}
 
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 * @todo set deleted flag
-	 */
-	public function actionDelete($id)
-	{
-		if(Yii::app()->request->isPostRequest) {
-			// we only allow deletion via POST request
-			// $this->loadModel($id)->delete();
-			
-
-			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-			if(!isset($_GET['ajax']))
-				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-		}
-		else
-			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
-	}
 
 	/**
 	 * Lists all models.
@@ -205,44 +131,4 @@ class CollectionController extends Controller
 		
 	}
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new Collection('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Collection']))
-			$model->attributes=$_GET['Collection'];
-
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
-
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer the ID of the model to be loaded
-	 */
-	public function loadModel($id)
-	{
-		$model=Collection::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}
-
-	/**
-	 * Performs the AJAX validation.
-	 * @param CModel the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='collection-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
 }
