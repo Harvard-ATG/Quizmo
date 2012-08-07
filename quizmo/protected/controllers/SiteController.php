d4268bcab2c57656a35de90aecd5f708c6319c07
jazahn <jcleveng@fas.harvard.edu>
2012-08-07 00:18:38 -0400

    added the error action back, good for getting forbidden errors

--- quizmo/protected/controllers/SiteController.php
+++ quizmo/protected/controllers/SiteController.php
@@ -55,6 +55,20 @@ class SiteController extends Controller
 	}
 	
 	/**
+	 * This is the action to handle external exceptions.
+	 */
+	public function actionError()
+	{
+	    if($error=Yii::app()->errorHandler->error)
+	    {
+	    	if(Yii::app()->request->isAjaxRequest)
+	    		echo $error['message'];
+	    	else
+	        	$this->render('error', $error);
+	    }
+	}
+	
+	/**
 	 * Displays the login page
 	 */
 	public function actionLogin()
