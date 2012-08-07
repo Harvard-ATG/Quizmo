88d04908566cc8129b76624c4cbb7c0af3e4c05a
jazahn <jcleveng@fas.harvard.edu>
2012-08-07 02:23:15 -0400

    ajax support for urls

--- quizmo/protected/smartyPlugins/function.url.php
+++ quizmo/protected/smartyPlugins/function.url.php
@@ -13,6 +13,7 @@
 function smarty_function_url($params, &$smarty){
 
 	$url = $params['url'];
+	$ajax = @$params['ajax'];
 	if(Yii::app()->params['authMethod'] == 'isites'){
 		
 		//$urlmanager = Yii::app()->getUrlManager();
@@ -20,8 +21,11 @@ function smarty_function_url($params, &$smarty){
 
 		// don't put the SCRIPT_URI in there, full url fails
 		//$url = $_SERVER['SCRIPT_URI'].$url;
-
-		$url = Yii::app()->isitestool->url($url);
+		if($ajax == 1){
+			$url = Yii::app()->isitestool->ajaxurl($url);
+		} else {
+			$url = Yii::app()->isitestool->url($url);
+		}
 
 		
 	}
