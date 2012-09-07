49af8b08bc7cd9d776b8d68fb1602a70264f4414
jazahn <jcleveng@fas.harvard.edu>
2012-09-07 15:59:35 -0400

    added a coursemanager extension stub and ignoring the coursemanager config file

---
+++ quizmo/protected/extensions/coursemanager/CourseManager.php
@@ -0,0 +1,18 @@
+<?php
+
+class CourseManager extends CApplicationComponent {
+	
+	public $coursemanagerdb;
+	
+	// This should be run once? when the collection is set up
+	public function setup(){
+		// get the list of students from the keyword:
+		
+		// add each student to the users table and the userscollections table
+		
+	}
+
+
+
+}	
+?>
