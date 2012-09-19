9b4702f97a2af7b1de5cc03947b29aa24fb5adf7
jazahn <jcleveng@fas.harvard.edu>
2012-09-19 00:31:59 -0400

    inserted the datatables js into the isites layout, this is currently done via php includes to avoid having to vpn to work in dev

--- quizmo/protected/views/layouts/isites.php
+++ quizmo/protected/views/layouts/isites.php
@@ -23,17 +23,7 @@ $host = "http://".$_SERVER['HTTP_HOST'];
 	<link href="<?php echo $host; ?>/css/bootstrap.css" rel="stylesheet" type="text/css"/>
     <link href="<?php echo $host; ?>/css/bootstrap-responsive.css" rel="stylesheet" type="text/css"/>
 -->
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
 
-    <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="<?php echo $host; ?>/ico/favicon.ico"/>
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $host; ?>/ico/apple-touch-icon-144-precomposed.png"/>
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $host; ?>/ico/apple-touch-icon-114-precomposed.png"/>
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $host; ?>/ico/apple-touch-icon-72-precomposed.png"/>
-    <link rel="apple-touch-icon-precomposed" href="<?php echo $host; ?>/ico/apple-touch-icon-57-precomposed.png"/>
 	
 	<script>
 	topicId = "<?php echo $_REQUEST['topicId']; ?>";
@@ -44,7 +34,12 @@ $host = "http://".$_SERVER['HTTP_HOST'];
     <!-- <script src="<?php echo $host; ?>/js/jquery-ui-datepicker.js"></script> -->
     <script src="http://twitter.github.com/bootstrap/assets/js/bootstrap.js"></script>
     <!-- <script src="<?php echo $host; ?>/js/bootstrap.js"></script> -->
-	
+	<script>
+	//<![CDATA[
+	<?php include("js/jquery.dataTables.js"); ?>
+	<?php include("js/jquery.dataTables.rowReordering.js"); ?>
+	//]]>
+	</script>
 
 		<div class="bootstrapped">
 		<?php echo $content; ?>
