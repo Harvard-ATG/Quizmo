e42727c724930fe9ed22c2708fc592412d12c498
jazahn <jcleveng@fas.harvard.edu>
2012-10-22 16:28:05 -0400

    added some emails to the fixtures

--- quizmo/protected/tests/fixtures/USERS.php
+++ quizmo/protected/tests/fixtures/USERS.php
@@ -21,24 +21,28 @@ return array(
 		'EXTERNAL_ID' => '12345678',
 		'FNAME' => 'Constantine',
 		'LNAME' => 'O\'Hara',
+		'EMAIL' => 'ohara@gmail.com'.
 	),
 	'user3' => array(
 		'NAME' => 'Snow F. White',
 		'EXTERNAL_ID' => '23456789',
 		'FNAME' => 'Snow',
 		'LNAME' => 'White',
+		'EMAIL' => 'snowwhite@gmail.com'.
 	),
 	'user4' => array(
 		'NAME' => 'Your F. Mother',
 		'EXTERNAL_ID' => '98765432f',
 		'FNAME' => 'Your',
 		'LNAME' => 'Mother',
+		'EMAIL' => 'mother@gmail.com'.
 	),
 	'user5' => array(
 		'NAME' => 'Tester McTesty',
 		'EXTERNAL_ID' => '345234526',
 		'FNAME' => 'Tester',
 		'LNAME' => 'McTesty',
+		'EMAIL' => 'tester@gmail.com'.
 	),
 	
 	
