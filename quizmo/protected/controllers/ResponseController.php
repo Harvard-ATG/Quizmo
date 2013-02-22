367345ebabcb934c86af01957aa12eff1acf1790
jazahn <jcleveng@fas.harvard.edu>
2013-02-22 10:46:43 -0500

    making grade available to all enrollees since it's used in individual response

--- quizmo/protected/controllers/ResponseController.php
+++ quizmo/protected/controllers/ResponseController.php
@@ -37,7 +37,7 @@ class ResponseController extends Controller
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('grade'),
-				'roles'=>array('admin','super'),
+				'roles'=>array('admin','super','enrollee'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
