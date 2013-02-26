eea70f9e1cb33589fc188f5ae0f39e5c24ab0d4d
jazahn <jcleveng@fas.harvard.edu>
2013-02-26 13:57:01 -0500

    using preg_quote instead of replacing pipe and using htmlspecialchars

--- quizmo/protected/smartyPlugins/function.fillin.php
+++ quizmo/protected/smartyPlugins/function.fillin.php
@@ -27,28 +27,22 @@ function smarty_function_fillin($params, &$smarty){
 		$disabled = ' disabled="disabled" ';
 	}
 	
-	// removing the pipes, they seem to have an odd effect on the inputs
-	$question = preg_replace("/\|/", "", $question);
 	preg_match_all("/\{[^}]*\}/", $question, $matches);
 	$responses_index = 0;
 	foreach($matches as $val){
 		foreach($val as $key => $match){
 			//error_log($match);
-			//if($responses == null){
 			if(!isset($responses[$responses_index])){
 				$new_input = "<input class='input-small fillin-text' type='text' $disabled/>";				
 			} else {
-				//error_log("$response...");
 				$response = $responses[$responses_index]['response'];
 				$responses_index++;
 				$new_input = "<input class='input-small fillin-text' type='text' value='$response' $disabled/>";
 			}
 			// if the pipe is left in, this preg_replace matches more times than we want because of the OR in the match... 
-			$question = preg_replace("/".htmlspecialchars($match)."/", $new_input, $question);
+			$question = preg_replace("/".preg_quote($match)."/", $new_input, $question);
 		}
-		//error_log(var_export($match, 1));
 	}
 	
-	error_log($question);
     return $question;
 }
