4238a0085a4746fb64f0aa772151091a09639272
jazahn <jcleveng@fas.harvard.edu>
2013-04-11 13:31:24 -0400

    reverting main to what should be on github

--- quizmo/protected/config/main.php
+++ quizmo/protected/config/main.php
@@ -22,18 +22,16 @@ return CMap::mergeArray(
 		),
 
 		// need to change this for isites
-		'layout'=>"isites",
+		'layout'=>"main",
 
 		'modules'=>array(
 			// uncomment the following to enable the Gii tool
-			/*
 			'gii'=>array(
 				'class'=>'system.gii.GiiModule',
 				'password'=>'giipassword',
 			 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 				'ipFilters'=>array('*'),
 			),
-			*/
 		),
 
 		// application components
@@ -51,7 +49,6 @@ return CMap::mergeArray(
 					'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 					'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 					'<controller:\w+>/<action:\w+>/<id:\d+>/<id2:\d+>'=>'<controller>/<action>',
-					'<controller:\w+>/<action:\w+>/<id:\d+>/<id2:\d+>/<id3:\d+>'=>'<controller>/<action>',
 					'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 					'<controller:\w+>/<action:\w+>/<cact:\w+>'=>'<controller>/<cact>',
 				),
@@ -82,7 +79,7 @@ return CMap::mergeArray(
 					//array(
 					//	'class'=>'CWebLogRoute',
 					//),
-					
+
 				),
 			),
 
@@ -100,7 +97,7 @@ return CMap::mergeArray(
 		    	//)
 			),
 
-			
+
 			'session' => array (
 	    		//'autoStart' => true,
 				'sessionName' => 'QUIZMO_SESSION',
@@ -124,8 +121,8 @@ return CMap::mergeArray(
 		'params'=>array(
 			// this is used in contact page
 			'adminEmail'=>'jcleveng@fas.harvard.edu',
-			//'authMethod'=>'facebook',
-			'authMethod'=>'isites',
+			'authMethod'=>'facebook',
+			//'authMethod'=>'isites',
 		),
 
 		// comment this if you don't want the login to be forced (if you want to allow viewer level guests)
@@ -140,9 +137,9 @@ return CMap::mergeArray(
 
 	),
 	require(dirname(__FILE__).'/database.php'),
-	//require(dirname(__FILE__).'/facebook.php'),
-	require(dirname(__FILE__).'/ldap.php'),
-	require(dirname(__FILE__).'/isites.php')
-	
+	require(dirname(__FILE__).'/facebook.php')
+	//require(dirname(__FILE__).'/ldap.php'),
+	//require(dirname(__FILE__).'/isites.php')
+
 );
 ?>
