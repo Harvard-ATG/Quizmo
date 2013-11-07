a121f81ba89d830a4c24b17ee6f03d92b3ed9507
jazahn <jcleveng@fas.harvard.edu>
2013-11-07 03:33:08 -0500

    adding jqueryui to the requirejs config

--- quizmo/js/main.js
+++ quizmo/js/main.js
@@ -3,10 +3,11 @@ requirejs.config({
 		'jquery' : 'http://code.jquery.com/jquery-1.10.1.min',
 		'lodash' : '//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.3.1/lodash',
 		'bootstrap' : '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min',
-		'jqueryui' : 'http://code.jquery.com/ui/1.8.23/jquery-ui.min.js'
+		'jqueryui' : 'http://code.jquery.com/ui/1.8.23/jquery-ui.min'
 	},
 	shim: {
 		'jquery' : '$',
-		'lodash' : '_'
+		'lodash' : '_',
+		'jqueryui' : ['jquery']
 	}
 });
