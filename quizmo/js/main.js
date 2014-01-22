0d9e51c922b648b49953b4412babad88216de7dc
jazahn <jcleveng@fas.harvard.edu>
2014-01-22 15:23:20 -0500

    changing jquery to local copies

--- quizmo/js/main.js
+++ quizmo/js/main.js
@@ -1,9 +1,9 @@
 requirejs.config({
 	paths: {
-		'jquery' : 'http://code.jquery.com/jquery-1.10.1.min',
+		'jquery' : 'jquery-1.7.2.min',
 		'lodash' : '//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.3.1/lodash',
 		'bootstrap' : '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min',
-		'jqueryui' : 'http://code.jquery.com/ui/1.8.23/jquery-ui.min'
+		'jqueryui' : 'jquery-ui'
 	},
 	shim: {
 		'jquery' : '$',
