183973118fd025f55efe809ce84c592def927558
jazahn <jcleveng@fas.harvard.edu>
2014-01-23 12:05:28 -0500

    adding jquery as a dep for bootstrap

--- quizmo/js/main.js
+++ quizmo/js/main.js
@@ -2,12 +2,15 @@ requirejs.config({
 	paths: {
 		'jquery' : 'jquery-1.7.2.min',
 		'lodash' : '//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.3.1/lodash',
-		'bootstrap' : '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min',
+		'bootstrap' : 'bootstrap',
 		'jqueryui' : 'jquery-ui'
 	},
 	shim: {
 		'jquery' : '$',
 		'lodash' : '_',
-		'jqueryui' : ['jquery']
+		'jqueryui' : ['jquery'],
+		'bootstrap' : {
+			deps: ['jquery']
+		},
 	}
 });
